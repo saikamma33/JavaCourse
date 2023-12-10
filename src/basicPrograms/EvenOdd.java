85291b47e32f0a1d6c6569c1881c17819fbe848f
saikamma33 <bhaskarkamma3@gmail.com>
2023-12-10 13:07:38 -0600

    add not example

--- src/basicPrograms/EvenOdd.java
+++ src/basicPrograms/EvenOdd.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class EvenOdd {
 
     public static void main (String[] args){
-System.out.println(" example ");
+System.out.println(" not example ");
         Scanner sc = new Scanner(System.in);
 
         System.out.println(" Enter the number ");
