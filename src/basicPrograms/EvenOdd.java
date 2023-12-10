6043e03955538d2c4bdf92750383ac0e39145bda
saikamma33 <bhaskarkamma3@gmail.com>
2023-12-10 13:02:31 -0600

    add sample

--- src/basicPrograms/EvenOdd.java
+++ src/basicPrograms/EvenOdd.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class EvenOdd {
 
     public static void main (String[] args){
-
+System.out.println(" sample ");
         Scanner sc = new Scanner(System.in);
 
         System.out.println(" Enter the number ");
